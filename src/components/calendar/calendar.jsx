2c2ef6bfd407ac1cb20bf9b0424c6e5c11fa5fcd
Jaeeun98 <wodms4525@naver.com>
2021-09-11 11:26:44 +0900

    Title : Calendar Daily Add
    Calendar Daily add

--- src/components/calendar/calendar.jsx
+++ src/components/calendar/calendar.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Daily from '../daily/daily';
+import Header from '../header/header';
 import Weekly from '../weekly/weekly';
 import styles from  './calendar.module.css'
 
+
 const Calendar = (props) => {
+
+    
+
     return(
         <section>
             <main className={styles.main}>
-                <header className={styles.header}>
-                    <button className={`${styles.btn} ${styles.leftBtn}`}>&#5176;</button>
-                        <h1 className={styles.title}>09</h1>
-                    <button className={`styles.btn styles.rightBtn`}>&#5171;</button>
-                </header>
-                
+                <Header />
                 <div className={styles.calendar}>
                     <div className={styles.daily}>
                         <Daily />
--- src/components/daily/daily.jsx
+++ src/components/daily/daily.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
+import styles from './daily.module.css'
+import dayjs from 'dayjs';
+import weekday from 'dayjs/plugin/weekday'
+import calendar from 'dayjs/plugin/calendar'
+
+const date = () => {
+    const start = dayjs().startOf('month').day();
+    const day = [];
+
+    for(let i=0; i<=start; i++){
+        day.push(
+            <li className={styles.li}></li>
+        )
+    }
+
+    for(let i=1; i<=30; i++){
+        day.push(
+            <li className={styles.li}>{i}</li>
+        )
+    }
+    
+    return day;
+}
 
 const Daily = (props) => {
+
+    
+    dayjs.extend(calendar)
     return(
-        <div>daily</div>
+        <>
+            
+            <ul className={styles.ul}>
+                <li className={`${styles.week} ${styles.li}`}>Mon</li>
+                <li className={`${styles.week} ${styles.li}`}>Tus</li>
+                <li className={`${styles.week} ${styles.li}`}>Wen</li>
+                <li className={`${styles.week} ${styles.li}`}>Thu</li>
+                <li className={`${styles.week} ${styles.li}`}>Fri</li>
+                <li className={`${styles.week} ${styles.li}`}>Sat</li>
+                <li className={`${styles.week} ${styles.li}`}>Sun</li>
+                {date()}
+            </ul>
+        </>
     )
 }
 
